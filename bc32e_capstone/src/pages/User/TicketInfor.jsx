b0c25f4a2582b79277d842e21da8d73408f31e14
BanhmiPate1311 <huynhcuong@gmail.com>
2022-11-03 21:59:52 +0700

    update fix

--- bc32e_capstone/src/pages/User/TicketInfor.jsx
+++ bc32e_capstone/src/pages/User/TicketInfor.jsx
@@ -20,7 +20,7 @@ const TicketInfor = () => {
         <div className="container px-5 py-4 mx-auto">
           <div className="flex flex-col text-center w-full mb-8">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-              Thông tin đặt vé
+              THÔNG TIN ĐẶT VÉ
             </h1>
           </div>
           <div className="flex flex-wrap -m-2">
@@ -40,15 +40,16 @@ const TicketInfor = () => {
                         {ticket.tenPhim}
                       </h2>
                       <div className="text-gray-500">
-                        Ngày chiếu:
+                        <span>Ngày chiếu: </span>
                         {moment(ticket.ngayDat).format("DD-MM-YYYY")}
                         <br />
-                        Giờ chiếu:
+                        <span>Giờ chiếu: </span>
                         {moment(ticket.ngayDat).format("hh-mm A")}
                         <br />
-                        Rạp: {seats.tenHeThongRap} - Ghế:{" "}
+                        <span>Rạp: </span>
+                        {seats.tenHeThongRap} <span>- Ghế: </span>
                         {ticket.danhSachGhe.map((ghe, index) => (
-                          <span className="text-blue-400" key={index}>
+                          <span className="text-blue-400 mr-2" key={index}>
                             {ghe.tenGhe}
                           </span>
                         ))}
