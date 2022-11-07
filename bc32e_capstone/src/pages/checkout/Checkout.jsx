5a5e77c881e9a50ca7ff450e2a6734a1fa55d300
khoa lnt <khoa201191@gmail.com>
2022-11-07 20:41:46 +0700

    fix checkout

--- bc32e_capstone/src/pages/checkout/Checkout.jsx
+++ bc32e_capstone/src/pages/checkout/Checkout.jsx
@@ -39,7 +39,7 @@ const Checkout = () => {
     return (
       <div className="container">
         <div>
-          <div className=" mt-4 flex text-center ml-[25%]">
+          <div className="mt-10 flex text-center ml-[25%] mb-28 sm:mb-28 lg:mb-32 loading-checkout">
             <Skeleton.Button
               className="mr-1"
               shape={buttonShape}
@@ -86,7 +86,7 @@ const Checkout = () => {
       return (
         <Fragment key={id}>
           <button
-            className={`seat ${classVipSeat} ${classAvailableSeat} ${classCheckedSeat} ${classSeatBy} `}
+            className={`seat ${classVipSeat} ${classAvailableSeat} ${classCheckedSeat} ${classSeatBy}`}
             onClick={() => dispatch(getCheckedList(seat))}
           >
             {seat.daDat ? (
@@ -107,99 +107,110 @@ const Checkout = () => {
   return (
     <div className="container mx-auto min-h-screen">
       <div className="grid grid-cols-12">
-        <div className="col-span-9">
-          <div></div>
+        <div className="checkout-seat-list col-span-7 xl:col-span-7 lg:col-span-8 md:col-span-9 sm:col-span-12">
           <div className="display-cinema">
             <h3 className="mb-0 text-red-50 text-lg">Màn hình</h3>
           </div>
-          <div>{renderSeat()}</div>
+          <div className="text-center">{renderSeat()}</div>
         </div>
-        <div className="col-span-3">
-          <h3 className="text-center text-2xl text-green-500">
-            <NumericFormat
-              className="text-center total-price-top"
-              allowLeadingZeros
-              thousandSeparator=","
-              value={checkedList.reduce((total, seat, i) => {
-                return (total += seat?.giaVe);
-              }, 0)}
-            />
-          </h3>
-          <hr />
-          <h3 className="text-xl">Phim: {seatList?.thongTinPhim?.tenPhim}</h3>
-          <p>Địa điểm: {seatList?.thongTinPhim?.diaChi}</p>
-          <p>Ngày chiếu: {seatList?.thongTinPhim?.ngayChieu}</p>
-          <hr />
-          <div className="my-5 flex flex-wrap">
-            <div className="w-full flex flex-wrap">
-              <span className="text-lg mr-3  infor-seat">Ghế: </span>
-              {_.sortBy(checkedList, ["maGhe"]).map((seat, i) => {
-                return (
-                  <span key={i} className="text-green-500 text-xl mr-2">
-                    {seat.stt}
-                  </span>
-                );
-              })}
+        <div className="checkout-infor col-span-4 xl:col-span-5 lg:col-span-4 md:col-span-3 sm:col-span-12">
+          <div>
+            <h3 className="text-center text-2xl text-green-500">
+              <NumericFormat
+                className="text-center total-price-top w-full"
+                allowLeadingZeros
+                thousandSeparator=","
+                value={checkedList.reduce((total, seat, i) => {
+                  return (total += seat?.giaVe);
+                }, 0)}
+              />
+              <span>VND</span>
+            </h3>
+            <hr />
+            <h3 className="text-xl">Phim: {seatList?.thongTinPhim?.tenPhim}</h3>
+            <p>Địa điểm: {seatList?.thongTinPhim?.diaChi}</p>
+            <p>Ngày chiếu: {seatList?.thongTinPhim?.ngayChieu}</p>
+            <hr />
+            <div className="my-5 flex flex-wrap">
+              <div className="w-full flex flex-wrap">
+                <span className="text-lg mr-3  infor-seat">Ghế: </span>
+                {_.sortBy(checkedList, ["maGhe"]).map((seat, i) => {
+                  return (
+                    <span key={i} className="text-green-500 text-xl mr-2">
+                      {seat.stt}
+                    </span>
+                  );
+                })}
+              </div>
+              <div className="text-right checkout-total-price flex ">
+                <span className="text-lg total">Total: </span>
+                <span className="text-green-400 text-lg">
+                  <NumericFormat
+                    className="total-price ml-3"
+                    allowLeadingZeros
+                    thousandSeparator=","
+                    value={checkedList.reduce((total, seat, i) => {
+                      return (total += seat.giaVe);
+                    }, 0)}
+                  />
+                </span>
+              </div>
             </div>
-            <div className="text-right checkout-total-price flex ">
-              <span className="text-lg total">Total: </span>
-              <span className="text-green-400 text-lg">
-                <NumericFormat
-                  className="text-end total-price mr-6"
-                  allowLeadingZeros
-                  thousandSeparator=","
-                  value={checkedList.reduce((total, seat, i) => {
-                    return (total += seat.giaVe);
-                  }, 0)}
-                />
-                <span className="mr-6 price-vnd">VND</span>
+            <hr />
+            <div className="my-5 checout-email">
+              <i className="infor-email">Email:</i>
+              <span className="text-green-400 text-xl ml-5">
+                {customer?.email}
               </span>
             </div>
-          </div>
-          <hr />
-          <div className="my-5 checout-email">
-            <i className="infor-email">Email:</i>
-            <span className="text-green-400 text-xl ml-5">
-              {customer?.email}
-            </span>
-          </div>
-          <hr />
-          <div className="my-5 checkout-phone">
-            <i className="infor-phone">Phone:</i>
-            <span className="text-green-400 text-xl ml-5">
-              {customer?.soDT}
-            </span>
-          </div>
-          <hr />
-          <div className="mb-0 flex flex-col justify-end">
-            <button
-              className="btn-checkout mt-3 w-full text-center py-3 font-bold text-lg"
-              onClick={() => {
-                const inforCheckOut = {
-                  maLichChieu: param.movieIds,
-                  danhSachVe: checkedList,
-                };
-                dispatch(postCheckOut(inforCheckOut));
-                // dispatch(getSeatList(param.movieIds));
-              }}
-            >
-              Đặt vé
-            </button>
-          </div>
-          <div className="mt-5 flex justify-center">
-            <div className="bg-white divide-y divide-gray-200">
-              <div className="text-black font-semibold">Ghế chưa đặt</div>
-              <button className="seat">00</button>
-              <div className="text-black font-semibold">Ghế đang đặt</div>
-              <button className="seat availableSeat">
-                <CloseOutlined />
+            <hr />
+            <div className="my-5 checkout-phone">
+              <i className="infor-phone">Phone:</i>
+              <span className="text-green-400 text-xl ml-5">
+                {customer?.soDT}
+              </span>
+            </div>
+            <hr />
+            <div className="mb-0 flex flex-col justify-end">
+              <button
+                className="btn-checkout mt-3 w-full text-center py-3 font-bold text-lg"
+                onClick={() => {
+                  const inforCheckOut = {
+                    maLichChieu: param.movieIds,
+                    danhSachVe: checkedList,
+                  };
+                  dispatch(postCheckOut(inforCheckOut));
+                  // dispatch(getSeatList(param.movieIds));
+                }}
+              >
+                Đặt vé
               </button>
-              <div className="text-black font-semibold">Ghế đã được đặt</div>
-              <button className="seat availableSeatBy">
-                <UserOutlined />
-              </button>
-              <div className="text-black font-semibold">Ghế VIP</div>
-              <button className="seat vipSeat">00</button>
+            </div>
+            <div className="mt-5 flex justify-center">
+              <div className="bg-white divide-y divide-gray-200 seat-types">
+                <div className="mr-3">
+                  <div className="text-black font-semibold">Ghế chưa đặt</div>
+                  <button className="seat">00</button>
+                </div>
+                <div className="mr-3">
+                  <div className="text-black font-semibold">Ghế đang đặt</div>
+                  <button className="seat availableSeat">
+                    <CloseOutlined />
+                  </button>
+                </div>
+                <div className="mr-3">
+                  <div className="text-black font-semibold">
+                    Ghế đã được đặt
+                  </div>
+                  <button className="seat availableSeatBy">
+                    <UserOutlined />
+                  </button>
+                </div>
+                <div className="mr-3">
+                  <div className="text-black font-semibold">Ghế VIP</div>
+                  <button className="seat vipSeat">00</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
