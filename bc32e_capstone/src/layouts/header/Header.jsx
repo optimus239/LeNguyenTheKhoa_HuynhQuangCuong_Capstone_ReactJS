52256f7cb97fde0c4af120e8ffd5de31857ce3d5
khoa lnt <khoa201191@gmail.com>
2023-04-17 10:39:35 +0700

    delete news contact

--- bc32e_capstone/src/layouts/header/Header.jsx
+++ bc32e_capstone/src/layouts/header/Header.jsx
@@ -85,25 +85,6 @@ const Header = () => {
                   </NavLink>
                 </span>
               </li>
-              <li className="nav-item">
-                <span className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75">
-                  <i className="text-lg text-white opacity-75"></i>
-                  <Link className="ml-2" to={"/news"}>
-                    Tin tức
-                  </Link>
-                </span>
-              </li>
-              <li className="nav-item">
-                <span
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="text-lg text-white opacity-75"></i>
-                  <Link className="ml-2" to={"/contact"}>
-                    Liên hệ
-                  </Link>
-                </span>
-              </li>
             </ul>
           </div>
           <div className="flex">
